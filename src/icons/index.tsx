db30891804a780f9501500bf09bf523fc40488af
Woochul Lee <leewoo0686@gmail.com>
2022-04-07 18:29:22 +0900

    chore: 새로운 아이콘 추가

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,3 +1,5 @@
+import CaretDown from './assets/caret-down.svg';
+import ChevronLeft from './assets/chevron-left.svg';
 import Close from './assets/close.svg';
 import Edit from './assets/edit-fill.svg';
 import EditOutline from './assets/edit-outline.svg';
@@ -8,11 +10,16 @@ import HeartOutline from './assets/heart-outline.svg';
 import Menu from './assets/menu.svg';
 import MoonFill from './assets/moon-fill.svg';
 import MoonOutline from './assets/moon-outline.svg';
+import Rising from './assets/rising.svg';
 import Search from './assets/search.svg';
+import Sort from './assets/sort.svg';
 import SunFill from './assets/sun-fill.svg';
 import SunOutline from './assets/sun-outline.svg';
+import Time from './assets/time.svg';
 
 export {
+  CaretDown,
+  ChevronLeft,
   Close,
   Edit,
   EditOutline,
@@ -23,7 +30,10 @@ export {
   Menu,
   MoonFill,
   MoonOutline,
+  Rising,
   Search,
+  Sort,
   SunFill,
   SunOutline,
+  Time,
 };

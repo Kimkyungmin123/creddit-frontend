3adb5b94107a7e372e7a2fe4f7980231ad9137a1
Kimkyungmin123 <rud3302@naver.com>
2022-05-03 11:31:35 +0900

    전역 관리 store폴더 삭제

--- src/store/types.ts
+++
@@ -1,5 +0,0 @@
-export const LOGIN_USER = 'login_user';
-export const SET_USER_INFO = 'set_user_info';
-export const NEW_CHAT = 'new_chat';
-export const SEND_MESSAGE = 'send_message';
-export const RECEIVE_MESSAGE = 'receive_message';
